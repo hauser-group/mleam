d5e291dfdbe27f6c9c5d1a4a0a985dbb77bc1415
Ralf Meyer <ralf.meyer@tugraz.at>
2020-08-10 17:14:12 +0200

    Setup of package directory

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlff',
+      version='0.1',
+      description='Machine learning based metallic force fields',
+      url='https://github.com/hauser-group/mlff',
+      author='Ralf Meyer',
+      author_email='meyer.ralf@yahoo.com',
+      packages=['mlff'])
