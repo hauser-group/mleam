fef6bd970eef4bfcdc357b9d47c33b50598939c9
Ralf Meyer <ralf.meyer@tugraz.at>
2020-08-11 11:27:42 +0200

    Fix falke8 styling

--- mlff/layers.py
+++ mlff/layers.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 
+
 class PolynomialCutoffFunction(tf.keras.layers.Layer):
-    
+
     def __init__(self, types, a=5.0, b=7.5, trainable=False, **kwargs):
         super().__init__(**kwargs)
         self.a = self.add_weight(
@@ -9,16 +10,17 @@ class PolynomialCutoffFunction(tf.keras.layers.Layer):
             initializer=tf.constant_initializer(a))
         self.b = self.add_weight(
             shape=(1), name='cut_b_' + types, trainable=trainable,
-            initializer=tf.constant_initializer(b))    
-    
+            initializer=tf.constant_initializer(b))
+
     @tf.function
     def call(self, r):
         r_scaled = (r - self.a)/(self.b - self.a)
         result = tf.where(
-            tf.logical_and(tf.greater(r, self.a), tf.less(r, self.b)), 
-            1.0 - 10.0 * r_scaled**3 + 15 * r_scaled**4 - 6 * r_scaled**5, tf.zeros_like(r))
+            tf.logical_and(tf.greater(r, self.a), tf.less(r, self.b)),
+            1.0 - 10.0 * r_scaled**3 + 15 * r_scaled**4 - 6 * r_scaled**5,
+            tf.zeros_like(r))
         return tf.where(tf.less_equal(r, self.a), tf.ones_like(r), result)
-        
+
 
 class InputNormalization(tf.keras.layers.Layer):
     """ Computes (r/r0 - 1). This is done in a separate layer in order
@@ -29,13 +31,14 @@ class InputNormalization(tf.keras.layers.Layer):
         self.r0 = self.add_weight(
             shape=(1), name='r0_' + types, trainable=trainable,
             initializer=tf.constant_initializer(r0))
-    
+
     @tf.function
     def call(self, r):
         return r/self.r0 - 1
 
+
 class BornMayer(tf.keras.layers.Layer):
-    
+
     def __init__(self, types, A=0.2, p=9.2, **kwargs):
         super().__init__(**kwargs)
         self.A = self.add_weight(
@@ -45,13 +48,14 @@ class BornMayer(tf.keras.layers.Layer):
             shape=(1), name='p_' + types,
             initializer=tf.constant_initializer(p))
         self._supports_ragged_inputs = True
-        
+
     @tf.function
     def call(self, r_normalized):
         return self.A*tf.exp(-self.p*r_normalized)
-    
+
+
 class RhoExp(tf.keras.layers.Layer):
-    
+
     def __init__(self, types, xi=1.6, q=3.5, **kwargs):
         super().__init__(**kwargs)
         self.xi = self.add_weight(
@@ -61,31 +65,33 @@ class RhoExp(tf.keras.layers.Layer):
             shape=(1), name='q_' + types,
             initializer=tf.constant_initializer(q))
         self._supports_ragged_inputs = True
-        
+
     @tf.function
     def call(self, r_normalized):
         return self.xi**2*tf.exp(-2*self.q*r_normalized)
-    
+
+
 class PairInteraction(tf.keras.layers.Layer):
     """Multiplies a pair-wise interaction with the cutoff function"""
-    
-    def __init__(self, input_normalization, 
+
+    def __init__(self, input_normalization,
                  pair_interaction, cutoff_function, **kwargs):
         super().__init__(**kwargs)
         self.input_normalization = input_normalization
         self.pair_interaction = pair_interaction
         self.cutoff_function = cutoff_function
-       
+
     @tf.function
     def call(self, r):
         return (self.pair_interaction(self.input_normalization(r))
                 * self.cutoff_function(r))
-    
+
+
 class SqrtEmbedding(tf.keras.layers.Layer):
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
+
     @tf.function
     def call(self, rho):
         return -tf.math.sqrt(rho)
--- mlff/models.py
+++ mlff/models.py
@@ -4,22 +4,24 @@ from mlff.layers import (PairInteraction, PolynomialCutoffFunction,
                          InputNormalization, BornMayer, RhoExp,
                          SqrtEmbedding)
 
+
 class EAMPotential(tf.keras.Model):
     """Base class for all EAM based potentials"""
-    
+
     def __init__(self, atom_types, build_forces=False):
         super().__init__()
-        
+
         self.atom_types = sorted(atom_types)
         self.atom_pair_types = []
         for (t1, t2) in combinations_with_replacement(self.atom_types, 2):
             self.atom_pair_types.append(''.join([t1, t2]))
         self.build_forces = build_forces
-        self.pair_potentials, self.pair_rho, self.embedding_functions = self.build_functions()
-        
+        (self.pair_potentials, self.pair_rho,
+         self.embedding_functions) = self.build_functions()
+
     def build_functions(self):
         pass
-    
+
     def body_partition_stitch(self, types, distances, pair_types):
         """main body using dynamic_partition and dynamic_stitch methods"""
         pair_type_indices = tf.dynamic_partition(
@@ -28,85 +30,107 @@ class EAMPotential(tf.keras.Model):
                 name='pair_type_indices')
         # Partition distances according to the pair_type
         partitioned_r = tf.dynamic_partition(
-            distances.flat_values * 1, pair_types.flat_values, 
+            distances.flat_values * 1, pair_types.flat_values,
             len(self.atom_pair_types), name='partitioned_r')
-        rho = [self.pair_rho[t](part) for t, part in zip(self.atom_pair_types, partitioned_r)]
-        phi = [self.pair_potentials[t](part) for t, part in zip(self.atom_pair_types, partitioned_r)]
+        rho = [self.pair_rho[t](part)
+               for t, part in zip(self.atom_pair_types, partitioned_r)]
+        phi = [self.pair_potentials[t](part)
+               for t, part in zip(self.atom_pair_types, partitioned_r)]
 
         rho = tf.expand_dims(tf.dynamic_stitch(pair_type_indices, rho * 1), -1)
         phi = tf.expand_dims(tf.dynamic_stitch(pair_type_indices, phi * 1), -1)
 
         # Reshape to ragged tensors
-        phi = tf.RaggedTensor.from_nested_row_splits(phi, distances.nested_row_splits)
-        rho = tf.RaggedTensor.from_nested_row_splits(rho, distances.nested_row_splits)
+        phi = tf.RaggedTensor.from_nested_row_splits(
+            phi, distances.nested_row_splits)
+        rho = tf.RaggedTensor.from_nested_row_splits(
+            rho, distances.nested_row_splits)
 
         # Sum over atoms j
         sum_rho = tf.reduce_sum(rho, axis=-2)
         sum_phi = tf.reduce_sum(phi, axis=-2)
 
         # Embedding energy
-        partitioned_sum_rho = tf.dynamic_partition(sum_rho * 1, types, len(self.atom_types), name='partitioned_sum_rho')
-        type_indices = tf.dynamic_partition(tf.expand_dims(tf.range(tf.size(sum_rho)), -1) * 1, types.flat_values , len(self.atom_types), name='type_indices')
-        embedding_energies = [self.embedding_functions[t](rho_t) for t, rho_t in zip(self.atom_types, partitioned_sum_rho)]
-        atomic_energies = (sum_phi.flat_values
-                           + tf.expand_dims(tf.dynamic_stitch(type_indices, embedding_energies * 1), -1))
+        partitioned_sum_rho = tf.dynamic_partition(
+            sum_rho * 1, types, len(self.atom_types),
+            name='partitioned_sum_rho')
+        type_indices = tf.dynamic_partition(
+            tf.expand_dims(tf.range(tf.size(sum_rho)), -1) * 1,
+            types.flat_values, len(self.atom_types), name='type_indices')
+        embedding_energies = [
+            self.embedding_functions[t](rho_t)
+            for t, rho_t in zip(self.atom_types, partitioned_sum_rho)]
+        atomic_energies = (
+            sum_phi.flat_values
+            + tf.expand_dims(
+                tf.dynamic_stitch(type_indices, embedding_energies * 1), -1))
         # Reshape to ragged
-        atomic_energies = tf.RaggedTensor.from_row_splits(atomic_energies, types.row_splits)
+        atomic_energies = tf.RaggedTensor.from_row_splits(
+            atomic_energies, types.row_splits)
         # Sum over atoms i
         return tf.reduce_sum(atomic_energies, axis=-2, name='energy')
-    
+
     def body_gather_scatter(self, types, distances, pair_types):
         """main body using gather and scatter methods"""
         phi = tf.zeros_like(distances.flat_values)
         rho = tf.zeros_like(distances.flat_values)
 
         for ij, type_ij in enumerate(self.atom_pair_types):
-            # Flat values necessary until where properly supports ragged tensors
-            indices = tf.where(tf.equal(pair_types, ij).flat_values)[:,0]
+            # Flat values necessary until where properly supports
+            # ragged tensors
+            indices = tf.where(tf.equal(pair_types, ij).flat_values)[:, 0]
             masked_distances = tf.gather(distances.flat_values, indices)
             phi = tf.tensor_scatter_nd_update(
-                phi, tf.expand_dims(indices, -1), self.pair_potentials[type_ij](masked_distances))
+                phi, tf.expand_dims(indices, -1),
+                self.pair_potentials[type_ij](masked_distances))
             rho = tf.tensor_scatter_nd_update(
-                rho, tf.expand_dims(indices, -1), self.pair_rho[type_ij](masked_distances))
+                rho, tf.expand_dims(indices, -1),
+                self.pair_rho[type_ij](masked_distances))
         # Reshape back to ragged tensors
-        phi = tf.RaggedTensor.from_nested_row_splits(phi, distances.nested_row_splits)
-        rho = tf.RaggedTensor.from_nested_row_splits(rho, distances.nested_row_splits)
+        phi = tf.RaggedTensor.from_nested_row_splits(
+            phi, distances.nested_row_splits)
+        rho = tf.RaggedTensor.from_nested_row_splits(
+            rho, distances.nested_row_splits)
         # Sum over atoms j and flatten again
         atomic_energies = tf.reduce_sum(phi, axis=-2).flat_values
         sum_rho = tf.reduce_sum(rho, axis=-2).flat_values
         for i, t in enumerate(self.atom_types):
-            indices = tf.where(tf.equal(types, i).flat_values)[:,0]
+            indices = tf.where(tf.equal(types, i).flat_values)[:, 0]
             atomic_energies = tf.tensor_scatter_nd_add(
                 atomic_energies, tf.expand_dims(indices, -1),
                 self.embedding_functions[t](tf.gather(sum_rho, indices)))
         # Reshape to ragged
-        atomic_energies = tf.RaggedTensor.from_row_splits(atomic_energies, types.row_splits)
+        atomic_energies = tf.RaggedTensor.from_row_splits(
+            atomic_energies, types.row_splits)
         # Sum over atoms i
         return tf.reduce_sum(atomic_energies, axis=-2, name='energy')
-                
+
+
 class DeepEAMPotential(EAMPotential):
-    
+
     def __init__(self, atom_types, **kwargs):
         super().__init__(atom_types, **kwargs)
-        
+
         types = tf.keras.Input(shape=(None, 1), ragged=True, dtype=tf.int32)
         positions = tf.keras.Input(shape=(None, 3), ragged=True)
         inputs = {'types': types, 'positions': positions}
-        
+
         self._set_inputs(inputs)
-    
-    #@tf.function
+
+    # @tf.function
     def call(self, inputs):
         types = inputs['types']
         positions = inputs['positions']
-        
+
         with tf.GradientTape() as tape:
             tape.watch(positions.flat_values)
-            #distances, pair_types = calc_distances_and_pair_types(positions, types, len(self.atom_types))
+            # distances, pair_types = calc_distances_and_pair_types(
+            #     positions, types, len(self.atom_types))
             distances = calc_distances(positions)
             pair_types = calc_pair_types(types, len(self.atom_types))
             energy = self.body_partition_stitch(types, distances, pair_types)
-        energy_per_atom = energy/tf.expand_dims(tf.cast(types.row_lengths(), tf.float32, name='number_of_atoms'), -1)
+        energy_per_atom = energy/tf.expand_dims(tf.cast(
+            types.row_lengths(), tf.float32, name='number_of_atoms'), -1)
 
         if self.build_forces:
             gradients = tf.RaggedTensor.from_row_splits(
@@ -115,57 +139,63 @@ class DeepEAMPotential(EAMPotential):
             return energy_per_atom, gradients
         return energy_per_atom
 
+
 class ShallowEAMPotential(EAMPotential):
-    
+
     def __init__(self, atom_types, **kwargs):
         super().__init__(atom_types, **kwargs)
         types = tf.keras.Input(shape=(None, 1), ragged=True, dtype=tf.int32)
         distances = tf.keras.Input(shape=(None, None, 1), ragged=True)
-        pair_types = tf.keras.Input(shape=(None, None, 1), ragged=True, dtype=tf.int32)
-        inputs = {'types': types, 'pair_types': pair_types, 'distances': distances}
+        pair_types = tf.keras.Input(shape=(None, None, 1), ragged=True,
+                                    dtype=tf.int32)
+        inputs = {'types': types, 'pair_types': pair_types,
+                  'distances': distances}
         if self.build_forces:
             # [batchsize] x N x (N-1) x N x 3
             dr_dx = tf.keras.Input(shape=(None, None, None, 3), ragged=True)
             inputs['dr_dx'] = dr_dx
-        
+
         self._set_inputs(inputs)
-    
-    #@tf.function
+
+    # @tf.function
     def call(self, inputs):
         types = inputs['types']
         distances = inputs['distances']
         pair_types = inputs['pair_types']
-        
+
         with tf.GradientTape() as tape:
             tape.watch(distances.flat_values)
             energy = self.body_partition_stitch(types, distances, pair_types)
-            #energy = self.body_gather_scatter(types, distances, pair_types)
-        energy_per_atom = energy/tf.expand_dims(tf.cast(types.row_lengths(), tf.float32, name='number_of_atoms'), axis=-1)
+            # energy = self.body_gather_scatter(types, distances, pair_types)
+        energy_per_atom = energy/tf.expand_dims(tf.cast(
+            types.row_lengths(), tf.float32, name='number_of_atoms'), axis=-1)
 
         if self.build_forces:
             dr_dx = inputs['dr_dx']
             dE_dr = tf.RaggedTensor.from_nested_row_splits(
                 tape.gradient(energy, distances.flat_values),
                 distances.nested_row_splits)
-            gradients = tf.reduce_sum(dr_dx * tf.expand_dims(dE_dr, -1), axis=(-3, -4))
+            gradients = tf.reduce_sum(dr_dx * tf.expand_dims(dE_dr, -1),
+                                      axis=(-3, -4))
             return energy_per_atom, gradients
         return energy_per_atom
 
+
 class SMATB(ShallowEAMPotential):
-    
+
     def __init__(self, atom_types, initial_params={},
                  r0_trainable=False, **kwargs):
         self.initial_params = initial_params
         self.r0_trainable = r0_trainable
         super().__init__(atom_types, **kwargs)
-    
+
     def build_functions(self):
         pair_potentials = {}
         pair_rho = {}
         for (t1, t2) in combinations_with_replacement(self.atom_types, 2):
             type_i = ''.join([t1, t2])
             normalized_input = InputNormalization(
-                type_i, r0=self.initial_params.get(('r0', type_i), 2.7), 
+                type_i, r0=self.initial_params.get(('r0', type_i), 2.7),
                 trainable=self.r0_trainable)
             cutoff_function = PolynomialCutoffFunction(
                 type_i, a=self.initial_params.get(('cut_a', type_i), 5.0),
@@ -173,13 +203,15 @@ class SMATB(ShallowEAMPotential):
             pair_potential = BornMayer(
                 type_i, A=self.initial_params.get(('A', type_i), 0.2),
                 p=self.initial_params.get(('p', type_i), 9.2))
-            rho = RhoExp(type_i, xi=self.initial_params.get(('xi', type_i), 1.6),
+            rho = RhoExp(type_i,
+                         xi=self.initial_params.get(('xi', type_i), 1.6),
                          q=self.initial_params.get(('q', type_i), 3.5))
             pair_potentials[type_i] = PairInteraction(
-                normalized_input, pair_potential, cutoff_function, name='%s-phi'%type_i)
+                normalized_input, pair_potential, cutoff_function,
+                name='%s-phi' % type_i)
             pair_rho[type_i] = PairInteraction(
-                normalized_input, rho, cutoff_function, name='%s-rho'%type_i)
+                normalized_input, rho, cutoff_function, name='%s-rho' % type_i)
         embedding_functions = {}
         for t in self.atom_types:
-            embedding_functions[t] = SqrtEmbedding(name='%s-Embedding'%t)
+            embedding_functions[t] = SqrtEmbedding(name='%s-Embedding' % t)
         return pair_potentials, pair_rho, embedding_functions
