e0a9e5b042a16d97966839521581ff26da90ac18
Ralf Meyer <ralf.meyer@tugraz.at>
2020-08-11 15:24:02 +0200

    Add pipeline factory

--- mlff/data_prep.py
+++ mlff/data_prep.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import json
 
 
 @tf.function
@@ -92,3 +93,46 @@ def distances_and_pair_types(xyz, types, n_types, cutoff=10.0):
         tf.reshape(gradient.flat_values, (-1, 3)),
         (row_lens, n*tf.ones(tf.reduce_sum(row_lens), dtype=tf.int64)))
     return distances, pair_types, gradient
+
+
+def dataset_from_json(path, type_dict, cutoff=10.0, batch_size=None,
+                      buffer_size=None):
+    with open(path, 'r') as fin:
+        data = json.load(fin)
+
+    if batch_size is None:
+        batch_size = len(data['symbols'])
+        buffer_size = batch_size
+    buffer_size = buffer_size or 4*batch_size
+
+    @tf.function
+    def input_transform(inp):
+        r, pair_types, dr_dx = distances_and_pair_types(
+            inp['positions'], inp['types'], 2, cutoff=cutoff)
+        return dict(types=inp['types'], pair_types=pair_types, distances=r,
+                    dr_dx=dr_dx)
+
+    input_dataset = tf.data.Dataset.from_tensor_slices(
+        {'types': tf.expand_dims(tf.ragged.constant(
+                [[type_dict[t] for t in syms] for syms in data['symbols']]),
+            axis=-1),
+         'positions': tf.ragged.constant(data['positions'], ragged_rank=1)})
+
+    input_dataset = input_dataset.map(
+        input_transform, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    N = tf.constant([len(sym) for sym in data['symbols']], dtype=tf.float32)
+    energy_per_atom = tf.constant(data['e_dft_bond'], dtype=tf.float32)/N
+    output_dataset = tf.data.Dataset.from_tensor_slices(
+        (energy_per_atom,
+         tf.ragged.constant(data['forces_dft'], ragged_rank=1)))
+
+    input_dataset = input_dataset.apply(
+        tf.data.experimental.dense_to_ragged_batch(batch_size=batch_size))
+    output_dataset = output_dataset.batch(batch_size=batch_size)
+
+    dataset = tf.data.Dataset.zip((input_dataset, output_dataset))
+
+    dataset = dataset.shuffle(buffer_size=buffer_size)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset
