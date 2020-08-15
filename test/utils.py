6a77171ab05c9ae675c171ff90f1f4c573dea1e6
Ralf Meyer <meyer.ralf@yahoo.com>
2020-08-16 00:26:10 +0200

    Initial commit of the numerical derivative util

--- test/utils.py
+++ test/utils.py
@@ -24,6 +24,31 @@ def rotation_matrix(n, alpha):
                    n[2]*n[2]*foo + cos_a]])
     return R
 
+def derive_scalar_wrt_array(fun, x0, dx=1e-4):
+    """fun: scalar function,
+       x0: np.array at which point the function should be derived"""
+    res = np.zeros_like(x0)
+    for i in range(x0.size):
+        ind = np.unravel_index(i, x0.shape)
+        displacement = np.zeros_like(x0)
+        displacement[ind] = dx
 
-def derivative_wrt_array(fun, x0, dx=1e-4):
-    return
+        f_plus = fun(x0 + displacement)
+        f_minus = fun(x0 - displacement)
+        res[ind] = (f_plus - f_minus)/(2*dx)
+    return res
+
+def derive_array_wrt_array(fun, x0, dx=1e-4):
+    """"""
+    f0 = fun(x0)
+    res = np.zeros(f0.shape + x0.shape)
+    for i in range(x0.size):
+        ind = np.unravel_index(i, x0.shape)
+        displacement = np.zeros_like(x0)
+        displacement[ind] = dx
+
+        f_plus = fun(x0 + displacement)
+        f_minus = fun(x0 - displacement)
+        # None is used as replacement for :
+        res[(None, None) + ind] = (f_plus - f_minus)/(2*dx)
+    return res
