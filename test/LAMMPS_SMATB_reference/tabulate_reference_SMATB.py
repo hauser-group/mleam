8f263f46713d3e6d733bff3a03116d72c785fb7b
Ralf Meyer <ralf.meyer@tugraz.at>
2020-08-25 16:54:35 +0200

    Test SMATB derivative versus Ferrando code and LAMMPS

---
+++ test/LAMMPS_SMATB_reference/tabulate_reference_SMATB.py
@@ -0,0 +1,108 @@
+import numpy as np
+
+from atsim.potentials import EAMPotential, Potential
+from atsim.potentials.eam_tabulation import SetFL_FS_EAMTabulation
+
+
+def cutoff(r, a, b):
+    if r <= a:
+        return 1.
+    elif r > b:
+        return 0.
+    else:
+        r_scaled = (r - a)/(b - a)
+        return (1.0 - 10.0 * r_scaled**3 + 15 * r_scaled**4
+                - 6 * r_scaled**5)
+
+
+def embedding_function(rho):
+    return -np.sqrt(rho)
+
+
+def make_density(xi, q, r_0, a, b):
+    def rho(r):
+        return xi**2 * np.exp(-2*q*(r/r_0 - 1)) * cutoff(r, a, b)
+    return rho
+
+
+def make_pair_pot(A, p, r_0, a, b):
+    """Factor two to compensate for the 1/2 in the definition"""
+    def phi(r):
+        return 2 * A * np.exp(-p*(r/r_0 - 1)) * cutoff(r, a, b)
+    return phi
+
+
+def main():
+    params = {
+        ('A', 'PtPt'): 0.1602, ('A', 'NiPt'): 0.1346, ('A', 'NiNi'): 0.0845,
+        ('xi', 'PtPt'): 2.1855, ('xi', 'NiPt'): 2.3338, ('xi', 'NiNi'): 1.405,
+        ('p', 'PtPt'): 13.00, ('p', 'NiPt'): 14.838, ('p', 'NiNi'): 11.73,
+        ('q', 'PtPt'): 3.13, ('q', 'NiPt'): 3.036, ('q', 'NiNi'): 1.93,
+        ('r0', 'PtPt'): 2.77, ('r0', 'NiPt'): 2.63, ('r0', 'NiNi'): 2.49,
+        ('cut_a', 'PtPt'): 4.08707719, ('cut_b', 'PtPt'): 5.0056268338740553,
+        ('cut_a', 'NiPt'): 4.08707719, ('cut_b', 'NiPt'): 4.4340500673763259,
+        ('cut_a', 'NiNi'): 3.62038672, ('cut_b', 'NiNi'): 4.4340500673763259}
+    # Create EAMPotential
+    eam_potentials = [EAMPotential('Ni', 28, 58.6934, embedding_function,
+                                   {'Ni': make_density(
+                                        params[('xi', 'NiNi')],
+                                        params[('q', 'NiNi')],
+                                        params[('r0', 'NiNi')],
+                                        params[('cut_a', 'NiNi')],
+                                        params[('cut_b', 'NiNi')]),
+                                    'Pt': make_density(
+                                        params[('xi', 'NiPt')],
+                                        params[('q', 'NiPt')],
+                                        params[('r0', 'NiPt')],
+                                        params[('cut_a', 'NiPt')],
+                                        params[('cut_b', 'NiPt')])},
+                                   latticeConstant=3.524, latticeType='fcc'),
+                      EAMPotential('Pt', 78, 195.084, embedding_function,
+                                   {'Ni': make_density(
+                                        params[('xi', 'NiPt')],
+                                        params[('q', 'NiPt')],
+                                        params[('r0', 'NiPt')],
+                                        params[('cut_a', 'NiPt')],
+                                        params[('cut_b', 'NiPt')]),
+                                    'Pt': make_density(
+                                        params[('xi', 'PtPt')],
+                                        params[('q', 'PtPt')],
+                                        params[('r0', 'PtPt')],
+                                        params[('cut_a', 'PtPt')],
+                                        params[('cut_b', 'PtPt')])},
+                                   latticeConstant=3.9242, latticeType='fcc')]
+    pair_potentials = [Potential('Ni', 'Ni',
+                                 make_pair_pot(params[('A', 'NiNi')],
+                                               params[('p', 'NiNi')],
+                                               params[('r0', 'NiNi')],
+                                               params[('cut_a', 'NiNi')],
+                                               params[('cut_b', 'NiNi')])),
+                       Potential('Ni', 'Pt',
+                                 make_pair_pot(params[('A', 'NiPt')],
+                                               params[('p', 'NiPt')],
+                                               params[('r0', 'NiPt')],
+                                               params[('cut_a', 'NiPt')],
+                                               params[('cut_b', 'NiPt')])),
+                       Potential('Pt', 'Pt',
+                                 make_pair_pot(params[('A', 'PtPt')],
+                                               params[('p', 'PtPt')],
+                                               params[('r0', 'PtPt')],
+                                               params[('cut_a', 'PtPt')],
+                                               params[('cut_b', 'PtPt')]))]
+
+    # Number of grid points and cut-offs for tabulation.
+    cutoff_rho = 100.0
+    nrho = 10000
+
+    cutoff = 6.0
+    nr = 10000
+
+    tabulation = SetFL_FS_EAMTabulation(
+        pair_potentials, eam_potentials, cutoff, nr, cutoff_rho, nrho)
+
+    with open("NiPt.eam.fs", "w") as outfile:
+        tabulation.write(outfile)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_smatb.py
@@ -0,0 +1,105 @@
+import unittest
+import json
+import numpy as np
+import tensorflow as tf
+from mlff.models import SMATB
+
+
+class SMATBTest(unittest.TestCase):
+
+    def test_versus_lammps(self):
+        # Read reference geometry
+        with open('LAMMPS_SMATB_reference/data.NiPt', 'r') as fin:
+            flag = False
+            # Convert to tensorflow types
+            type_dict = {'1': 0, '2': 1}
+            types = []
+            positions = []
+            for line in fin:
+                if line.startswith('Atoms # atomic'):
+                    # Skip one line:
+                    line = next(fin)
+                    flag = True
+                elif flag:
+                    sp = line.split()
+                    types.append(type_dict[sp[1]])
+                    positions.append([float(s) for s in sp[2:5]])
+        N = len(types)
+        # Read LAMMPS energy
+        with open('LAMMPS_SMATB_reference/log.lammps', 'r') as fin:
+            for line in fin:
+                if line.startswith('Step Temp E_pair E_mol TotEng Press'):
+                    sp = next(fin).split()
+                    e_ref = float(sp[4])
+        # Read LAMMPS forces:
+        with open('LAMMPS_SMATB_reference/dump.force', 'r') as fin:
+            flag = False
+            forces_ref = []
+            for line in fin:
+                if line.startswith('ITEM: ATOMS id type fx fy fz'):
+                    flag = True
+                elif flag:
+                    sp = line.split()
+                    forces_ref.append([float(s) for s in sp[2:5]])
+
+        params = {
+            ('A', 'PtPt'): 0.1602, ('A', 'NiPt'): 0.1346,
+            ('A', 'NiNi'): 0.0845, ('xi', 'PtPt'): 2.1855,
+            ('xi', 'NiPt'): 2.3338, ('xi', 'NiNi'): 1.405,
+            ('p', 'PtPt'): 13.00, ('p', 'NiPt'): 14.838, ('p', 'NiNi'): 11.73,
+            ('q', 'PtPt'): 3.13, ('q', 'NiPt'): 3.036, ('q', 'NiNi'): 1.93,
+            ('r0', 'PtPt'): 2.77, ('r0', 'NiPt'): 2.63, ('r0', 'NiNi'): 2.49,
+            ('cut_a', 'PtPt'): 4.08707719,
+            ('cut_b', 'PtPt'): 5.0056268338740553,
+            ('cut_a', 'NiPt'): 4.08707719,
+            ('cut_b', 'NiPt'): 4.4340500673763259,
+            ('cut_a', 'NiNi'): 3.62038672,
+            ('cut_b', 'NiNi'): 4.4340500673763259}
+
+        model = SMATB(['Ni', 'Pt'], params=params, build_forces=True)
+
+        types = tf.expand_dims(tf.ragged.constant([types]), axis=2)
+        positions = tf.ragged.constant([positions], ragged_rank=1)
+        energy, forces = model({'types': types, 'positions': positions})
+
+        np.testing.assert_allclose(energy.numpy()[0]*N, e_ref)
+        np.testing.assert_allclose(forces.numpy()[0], forces_ref, atol=1e-5)
+
+    def test_versus_ferrando_code(self):
+
+        with open('../data/test_data.json', 'r') as fin:
+            data = json.load(fin)
+
+        type_dict = {'Ni': 0, 'Pt': 1}
+        e_ref = data['e_smatb']
+
+        types = tf.ragged.constant(
+            [[[type_dict[ti]] for ti in type_vec]
+             for type_vec in data['symbols']], ragged_rank=1)
+        positions = tf.ragged.constant(data['positions'], ragged_rank=1)
+        Ns = tf.cast(types.row_lengths(), dtype=tf.float32)
+
+        params = {
+            ('A', 'PtPt'): 0.1602, ('A', 'NiPt'): 0.1346,
+            ('A', 'NiNi'): 0.0845, ('xi', 'PtPt'): 2.1855,
+            ('xi', 'NiPt'): 2.3338, ('xi', 'NiNi'): 1.405,
+            ('p', 'PtPt'): 13.00, ('p', 'NiPt'): 14.838, ('p', 'NiNi'): 11.73,
+            ('q', 'PtPt'): 3.13, ('q', 'NiPt'): 3.036, ('q', 'NiNi'): 1.93,
+            ('r0', 'PtPt'): 2.77, ('r0', 'NiPt'): 2.63, ('r0', 'NiNi'): 2.49,
+            ('cut_a', 'PtPt'): 4.08707719,
+            ('cut_b', 'PtPt'): 5.0056268338740553,
+            ('cut_a', 'NiPt'): 4.08707719,
+            ('cut_b', 'NiPt'): 4.4340500673763259,
+            ('cut_a', 'NiNi'): 3.62038672,
+            ('cut_b', 'NiNi'): 4.4340500673763259}
+
+        model = SMATB(['Ni', 'Pt'], params=params, build_forces=True)
+
+        e_model, _ = model({'types': types, 'positions': positions})
+        e_model = tf.squeeze(e_model)*Ns
+
+        np.testing.assert_allclose(e_model.numpy(), e_ref, rtol=1e-2)
+
+
+if __name__ == '__main__':
+    unittest.main()
