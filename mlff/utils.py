f8e29f9835a4b3473a48b2f50e51b34efb72a1cc
Ralf Meyer <ralf.meyer@tugraz.at>
2020-09-14 13:16:49 +0200

    add LBFGS optimization function

--- mlff/utils.py
+++ mlff/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -109,3 +110,105 @@ class ConstantExponentialDecay(tf.keras.optimizers.schedules.ExponentialDecay):
         config = super().get_config()
         config.update({'constant_steps': self.constant_steps})
         return config
+
+
+def opt_fun_factory(model, loss, train_x, train_y, val_x=None, val_y=None,
+                    save_path=None):
+    """A factory to create a function required by tfp.optimizer.lbfgs_minimize.
+    Args:
+        model [in]: an instance of `tf.keras.Model` or its subclasses.
+        loss [in]: a function with signature loss_value = loss(pred_y, true_y).
+        train_x [in]: the input part of training data.
+        train_y [in]: the output part of training data.
+    Returns:
+        A function that has a signature of:
+            loss_value, gradients = f(model_parameters).
+    """
+
+    # obtain the shapes of all trainable parameters in the model
+    shapes = tf.shape_n(model.trainable_variables)
+    n_tensors = len(shapes)
+
+    # we'll use tf.dynamic_stitch and tf.dynamic_partition later, so we need to
+    # prepare required information first
+    count = 0
+    idx = []  # stitch indices
+    part = []  # partition indices
+
+    for i, shape in enumerate(shapes):
+        n = np.product(shape)
+        idx.append(tf.reshape(tf.range(count, count+n, dtype=tf.int32), shape))
+        part.extend([i]*n)
+        count += n
+
+    part = tf.constant(part)
+
+    @tf.function
+    def assign_new_model_parameters(params_1d):
+        """A function updating the model's parameters with a 1D tf.Tensor.
+        Args:
+            params_1d [in]: a 1D tf.Tensor representing the model's trainable
+            parameters.
+        """
+
+        params = tf.dynamic_partition(params_1d, part, n_tensors)
+        for i, (shape, param) in enumerate(zip(shapes, params)):
+            model.trainable_variables[i].assign(tf.reshape(param, shape))
+
+    # now create a function that will be returned by this factory
+    @tf.function
+    def f(params_1d):
+        """A function that can be used by tfp.optimizer.lbfgs_minimize.
+        This function is created by function_factory.
+        Args:
+           params_1d [in]: a 1D tf.Tensor.
+        Returns:
+            A scalar loss and the gradients w.r.t. the `params_1d`.
+        """
+
+        # use GradientTape so that we can calculate the gradient of loss w.r.t.
+        # parameters
+        with tf.GradientTape() as tape:
+            # update the parameters in the model
+            assign_new_model_parameters(params_1d)
+            # calculate the loss
+            loss_value = loss(model(train_x, training=True), train_y)
+
+        # calculate gradients and convert to 1D tf.Tensor
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        grads = tf.dynamic_stitch(idx, grads)
+
+        if f.val:
+            val_loss = loss(model(val_x), val_y)
+            if val_loss < f.best_val:
+                tf.py_function(
+                    lambda epoch, val_loss:
+                        model.save_weights(save_path.format(epoch=epoch,
+                                                            val_loss=val_loss)
+                                           ),
+                    inp=[f.iter, val_loss], Tout=[])
+                f.best_val.assign(val_loss)
+        else:
+            val_loss = 0.0
+
+        # print out iteration & loss
+        f.iter.assign_add(1)
+        tf.print("Iter:", f.iter, "train_loss:", loss_value,
+                 'val_loss:', val_loss)
+
+        # store loss value so we can retrieve later
+        tf.py_function(f.history.append, inp=[(loss_value, val_loss)], Tout=[])
+
+        return loss_value, grads
+
+    # store these information as members so we can use them outside the scope
+    f.iter = tf.Variable(0)
+    f.idx = idx
+    f.part = part
+    f.shapes = shapes
+    f.assign_new_model_parameters = assign_new_model_parameters
+    f.history = []
+    f.val = val_x is not None and val_y is not None
+    f.best_val = tf.Variable(1e10)
+
+    return f
