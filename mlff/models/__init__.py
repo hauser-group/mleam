bc18d8963cc31b6683f0a748238bf214eebd837d
Ralf Meyer <ralf.meyer@tugraz.at>
2020-12-02 17:52:01 +0100

    Fix missing __init__ for ExtendedEmbeddingV2Model

--- mlff/models/__init__.py
+++ mlff/models/__init__.py
@@ -1,10 +1,11 @@
-from .eam_models import (SMATB, ExtendedEmbeddingModel, NNEmbeddingModel,
+from .eam_models import (SMATB, ExtendedEmbeddingModel,
+                         ExtendedEmbeddingV2Model, NNEmbeddingModel,
                          RhoTwoExpModel, NNRhoModel, NNRhoExpModel,
                          ExtendedEmbeddingRhoTwoExpModel,
                          NNEmbeddingNNRhoModel, NNEmbeddingNNRhoExpModel)
 from .bp_model import BehlerParrinello
 
-__all__ = ['SMATB', 'ExtendedEmbeddingModel', 'NNEmbeddingModel',
-           'RhoTwoExpModel', 'NNRhoModel', 'NNRhoExpModel',
+__all__ = ['SMATB', 'ExtendedEmbeddingModel', 'ExtendedEmbeddingV2Model',
+           'NNEmbeddingModel', 'RhoTwoExpModel', 'NNRhoModel', 'NNRhoExpModel',
            'ExtendedEmbeddingRhoTwoExpModel', 'NNEmbeddingNNRhoModel',
            'NNEmbeddingNNRhoExpModel', 'BehlerParrinello']
