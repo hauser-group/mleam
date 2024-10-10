2d02b6561577310b3e793f460e6596c40c108360
Ralf Meyer <meyer.ralf@yahoo.com>
2024-10-10 12:57:06 +0200

    Support for batched inputs in preprocessing

--- mleam/preprocessing.py
+++ mleam/preprocessing.py
@@ -1,45 +1,35 @@
 import tensorflow as tf
 
 
-def dense_distances_and_pair_types(xyzs, types, n_types, diagonal=0.0):
-    distances, gradient = get_distance_matrix_and_derivative(xyzs)
+def distances_and_pair_types(xyzs, types, n_types, diagonal=0.0):
+    distances, gradient = get_distance_matrix_and_derivative(xyzs, diagonal=diagonal)
     pair_types = get_pair_types(types, n_types)
 
     return distances, pair_types, gradient
 
 
-def distances_and_pair_types(xyzs, types, n_types, cutoff=10.0):
-    cutoff = tf.cast(cutoff, dtype=xyzs.dtype)
-
-    n = tf.shape(xyzs, out_type=tf.int64)[0]
-    distances, pair_types, gradient = dense_distances_and_pair_types(
-        xyzs, types, n_types
-    )
+def ragged_distances_and_pair_types(xyzs, types, n_types, cutoff=10.0):
+    distances, pair_types, derivative = distances_and_pair_types(xyzs, types, n_types)
+    tf.print(distances.shape, pair_types.shape, derivative.shape)
     mask = tf.logical_and(
-        tf.logical_not(tf.eye(n, dtype=tf.bool)),
-        tf.less_equal(distances[:, :, 0], cutoff),
+        tf.less_equal(distances[:, :, 0], cutoff, name="mask_max_cutoff"),
+        tf.greater(distances[:, :, 0], 0.0),
     )
 
     distances = tf.ragged.boolean_mask(distances, mask)
     pair_types = tf.ragged.boolean_mask(pair_types, mask)
-    gradient = tf.ragged.boolean_mask(gradient, mask)
-    # convert into ragged_rank=2
-    row_lengths = gradient.row_lengths()
-    gradient = tf.RaggedTensor.from_nested_row_lengths(
-        tf.reshape(gradient.flat_values, (-1, 3)),
-        (row_lengths, n * tf.ones(tf.reduce_sum(row_lengths), dtype=tf.int64)),
-    )
-    return distances, pair_types, gradient
+    derivative = tf.ragged.boolean_mask(derivative, mask)
+    return distances, pair_types, derivative
 
 
-def dense_distances_and_pair_types_no_grad(xyzs, types, n_types, diagonal=0.0):
+def distances_and_pair_types_no_grad(xyzs, types, n_types, diagonal=0.0):
     pair_types = get_pair_types(types, n_types)
     distances = get_distance_matrix(xyzs)
     return distances, pair_types
 
 
-def distances_and_pair_types_no_grad(xyzs, types, n_types, cutoff=10.0):
-    distances, pair_types = dense_distances_and_pair_types_no_grad(xyzs, types, n_types)
+def ragged_distances_and_pair_types_no_grad(xyzs, types, n_types, cutoff=10.0):
+    distances, pair_types = distances_and_pair_types_no_grad(xyzs, types, n_types)
     mask = tf.logical_and(
         tf.less_equal(distances[:, :, 0], cutoff, name="mask_max_cutoff"),
         tf.greater(distances[:, :, 0], 0.0),
@@ -63,38 +53,45 @@ def get_pair_types(types, n_types):
     # A bond of type A-C would therefore be assigned the integer 2.
     # First figure out the column we are in (essentially just sorting the
     # two involved pair types)
-    min_ij = tf.math.minimum(types[:, tf.newaxis], types[tf.newaxis, :])
+    min_ij = tf.math.minimum(tf.expand_dims(types, axis=-2), tf.expand_dims(types, -3))
     # TODO: is the second line really needed here? Should be generalized to
     # asymmetric functions anyway
     return (
         n_types * min_ij
         - (min_ij * (min_ij - 1)) // 2
-        + tf.abs(tf.subtract(types[:, tf.newaxis], types[tf.newaxis, :]))
+        + tf.abs(tf.expand_dims(types, axis=-2) - tf.expand_dims(types, axis=-3))
     )
 
 
 def get_distance_matrix(xyzs, diagonal=0.0):
-    n = tf.shape(xyzs, out_type=tf.int64)[0]
-    r_vec = tf.expand_dims(xyzs, 0) - tf.expand_dims(xyzs, 1)
+    r_vec = tf.expand_dims(xyzs, -3) - tf.expand_dims(xyzs, -2)
     distances = tf.sqrt(
         tf.reduce_sum(r_vec**2, axis=-1, name="sum_distances"),
         name="distance_computation",
     )
+    if isinstance(xyzs, tf.RaggedTensor):
+        diagonals = tf.ragged.stack(
+            [diagonal * tf.eye(n, dtype=xyzs.dtype) for n in xyzs.row_lengths()]
+        )
+    else:
+        diagonals = diagonal * tf.eye(
+            tf.shape(xyzs)[-2], dtype=xyzs.dtype, batch_shape=tf.shape(xyzs)[:-2]
+        )
     distances = tf.expand_dims(
-        tf.linalg.set_diag(distances, diagonal * tf.ones(n, dtype=xyzs.dtype)),
+        distances + diagonals,
         axis=-1,
     )
     return distances
 
 
-def get_distance_matrix_and_compact_derivative(xyzs, diagonal=0.0):
+def get_distance_matrix_and_derivative(xyzs, diagonal=0.0):
     """
-    Importantly, this only returns the "compact" form of the derivative, i.e,
+    Importantly, this only returns the "compact" 3 dimensional form of the derivative:
     $$
     \\frac{(x_{j,\\xi} - x_{i,\\xi})}{r_{ij}}
     $$
-    To get the full 4 dimensional matrix the 3 dimensional gradient obtained here
-    has to be multiplied by a sum of kronecker deltas:
+    To get the full 4 dimensional matrix the 3 dimensional derivative obtained here
+    has to be multiplied by a difference of two kronecker deltas:
     $$
     \\frac{\\partial r_{ij}}{x_{k,\\xi}} = \\frac{(x_{j,\\xi} - x_{i,\\xi})}{r_{ij}}
     (\\delta_{jk} - \\delta_{ik})
@@ -112,32 +109,34 @@ def get_distance_matrix_and_compact_derivative(xyzs, diagonal=0.0):
     >>> full_derivative.shape
     TensorShape([2, 2, 2, 3])
     """
-    n = tf.shape(xyzs, out_type=tf.int64)[0]
-    r_vec = tf.expand_dims(xyzs, 0) - tf.expand_dims(xyzs, 1)
+    r_vec = tf.expand_dims(xyzs, -3) - tf.expand_dims(xyzs, -2)
     distances = tf.sqrt(tf.reduce_sum(r_vec**2, axis=-1))
+    if isinstance(xyzs, tf.RaggedTensor):
+        diagonals = tf.ragged.stack(
+            [diagonal * tf.eye(n, dtype=xyzs.dtype) for n in xyzs.row_lengths()]
+        )
+    else:
+        diagonals = diagonal * tf.eye(
+            tf.shape(xyzs)[-2], dtype=xyzs.dtype, batch_shape=tf.shape(xyzs)[:-2]
+        )
     distances = tf.expand_dims(
-        tf.linalg.set_diag(distances, diagonal * tf.ones(n, dtype=xyzs.dtype)),
+        distances + diagonals,
         axis=-1,
     )
-    gradient = tf.math.divide_no_nan(r_vec, distances)
+    derivative = tf.math.divide_no_nan(r_vec, distances)
 
-    return distances, gradient
+    return distances, derivative
 
 
-def get_distance_matrix_and_derivative(xyzs, diagonal=0.0):
+def get_distance_matrix_and_full_derivative(xyzs, diagonal=0.0):
     n = tf.shape(xyzs, out_type=tf.int64)[0]
-    r_vec = tf.expand_dims(xyzs, 0) - tf.expand_dims(xyzs, 1)
-    distances = tf.sqrt(tf.reduce_sum(r_vec**2, axis=-1))
-    distances = tf.expand_dims(
-        tf.linalg.set_diag(distances, diagonal * tf.ones(n, dtype=xyzs.dtype)),
-        axis=-1,
-    )
+    distances, derivative = get_distance_matrix_and_derivative(xyzs, diagonal=diagonal)
 
-    # The full gradient is of shape (n, n, n, 3) indexed as d r_{i,j} / d x_{k,l}
+    # The full derivative is of shape (n, n, n, 3) indexed as d r_{i,j} / d x_{k,l}
     # and calculated as r_vec_{i,j,l} (kron_{j,k} - kron_{i,k})
-    gradient = tf.math.divide_no_nan(r_vec, distances)[:, :, tf.newaxis, :] * (
-        tf.eye(n, dtype=xyzs.dtype)[tf.newaxis, :, :, tf.newaxis]
-        - tf.eye(n, dtype=xyzs.dtype)[:, tf.newaxis, :, tf.newaxis]
+    derivative = tf.expand_dims(derivative, axis=-2) * (
+        tf.expand_dims(tf.expand_dims(tf.eye(n, dtype=xyzs.dtype), axis=-3), axis=-1)
+        - tf.expand_dims(tf.expand_dims(tf.eye(n, dtype=xyzs.dtype), axis=-2), axis=-1)
     )
 
-    return distances, gradient
+    return distances, derivative
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -6,6 +6,8 @@ from mleam.preprocessing import (
     get_pair_types,
     get_distance_matrix,
     get_distance_matrix_and_derivative,
+    get_distance_matrix_and_full_derivative,
+    distances_and_pair_types,
 )
 from scipy.spatial.transform import Rotation
 
@@ -21,18 +23,63 @@ def types():
     return tf.expand_dims(tf.constant([0, 0, 1, 1]), axis=-1)
 
 
-def test_distance_matrix(xyzs, types, atol=1e-6):
-    r = get_distance_matrix(xyzs)
+@pytest.fixture
+def batched_xyzs():
+    # 3 and 4 to result in Pythagorean triples
+    return tf.constant(
+        [
+            [
+                [0, 0, 0],
+                [3.0, 0, 0],
+                [0, 4.0, 0],
+                [0, 3.0, 4.0],
+            ],
+            [
+                [0, 0, 0],
+                [3.0, 0, 0],
+                [0, 4.0, 0],
+                [0, 4.0, 3.0],
+            ],
+        ],
+    )
+
+
+@pytest.fixture
+def batched_types():
+    return tf.expand_dims(tf.constant([[0, 0, 1, 1], [0, 0, 0, 1]]), axis=-1)
+
+
+@pytest.fixture
+def ragged_xyzs():
+    # 3 and 4 to result in Pythagorean triples
+    return tf.ragged.constant(
+        [
+            [[0, 0, 0], [3.0, 0, 0], [0, 4.0, 0], [0, 3.0, 4.0]],
+            [[0, 0, 0], [3.0, 0, 0], [0, 4.0, 0]],
+        ],
+        ragged_rank=1,
+    )
+
+
+@pytest.fixture
+def ragged_types():
+    return tf.expand_dims(
+        tf.ragged.constant([[0, 0, 1, 1], [0, 0, 1]], ragged_rank=1), axis=-1
+    )
+
+
+def test_distance_matrix(xyzs, atol=1e-6):
+    r = get_distance_matrix(xyzs, diagonal=8.1)
 
     np.testing.assert_allclose(
         r,
         np.expand_dims(
             np.array(
                 [
-                    [0.0, 3.0, 4.0, 5.0],
-                    [3.0, 0.0, 5.0, np.sqrt(34)],
-                    [4.0, 5.0, 0.0, np.sqrt(17)],
-                    [5.0, np.sqrt(34), np.sqrt(17), 0.0],
+                    [8.1, 3.0, 4.0, 5.0],
+                    [3.0, 8.1, 5.0, np.sqrt(34)],
+                    [4.0, 5.0, 8.1, np.sqrt(17)],
+                    [5.0, np.sqrt(34), np.sqrt(17), 8.1],
                 ]
             ),
             axis=-1,
@@ -40,17 +87,61 @@ def test_distance_matrix(xyzs, types, atol=1e-6):
     )
 
 
-def test_pair_types(types):
-    # Test correct pair types
-    # (0, 0) -> 0
-    # (0, 1), (1, 0) -> 1
-    # (1, 1) -> 2
-    # Pair_type matrix:
-    # 0 0 1 1
-    # 0 0 1 1
-    # 1 1 2 2
-    # 1 1 2 2
+def test_batched_distance_matrix(batched_xyzs, atol=1e-6):
+    r = get_distance_matrix(batched_xyzs, 8.1)
 
+    np.testing.assert_allclose(
+        r[0, :, :, 0].numpy(),
+        np.array(
+            [
+                [8.1, 3.0, 4.0, 5.0],
+                [3.0, 8.1, 5.0, np.sqrt(34)],
+                [4.0, 5.0, 8.1, np.sqrt(17)],
+                [5.0, np.sqrt(34), np.sqrt(17), 8.1],
+            ]
+        ),
+    )
+    np.testing.assert_allclose(
+        r[1, :, :, 0].numpy(),
+        np.array(
+            [
+                [8.1, 3.0, 4.0, 5.0],
+                [3.0, 8.1, 5.0, np.sqrt(34)],
+                [4.0, 5.0, 8.1, 3.0],
+                [5.0, np.sqrt(34), 3.0, 8.1],
+            ]
+        ),
+    )
+
+
+def test_ragged_batch_distance_matrix(ragged_xyzs, atol=1e-6):
+    r = get_distance_matrix(ragged_xyzs, 8.1)
+
+    np.testing.assert_allclose(
+        r[0, :, :, 0].numpy(),
+        np.array(
+            [
+                [8.1, 3.0, 4.0, 5.0],
+                [3.0, 8.1, 5.0, np.sqrt(34)],
+                [4.0, 5.0, 8.1, np.sqrt(17)],
+                [5.0, np.sqrt(34), np.sqrt(17), 8.1],
+            ]
+        ),
+    )
+
+    np.testing.assert_allclose(
+        r[1, :, :, 0].numpy(),
+        np.array(
+            [
+                [8.1, 3.0, 4.0],
+                [3.0, 8.1, 5.0],
+                [4.0, 5.0, 8.1],
+            ]
+        ),
+    )
+
+
+def test_pair_types(types):
     pair_types = get_pair_types(types, 2)
 
     np.testing.assert_array_equal(
@@ -65,6 +156,33 @@ def test_pair_types(types):
                 ]
             ),
             axis=-1,
+        ),
+    )
+
+
+def test_batched_pair_types(batched_types):
+    pair_types = get_pair_types(batched_types, 2)
+
+    np.testing.assert_array_equal(
+        pair_types[0, :, :, 0],
+        np.array(
+            [
+                [0, 0, 1, 1],
+                [0, 0, 1, 1],
+                [1, 1, 2, 2],
+                [1, 1, 2, 2],
+            ]
+        ),
+    )
+    np.testing.assert_array_equal(
+        pair_types[1, :, :, 0],
+        np.array(
+            [
+                [0, 0, 0, 1],
+                [0, 0, 0, 1],
+                [0, 0, 0, 1],
+                [1, 1, 1, 2],
+            ]
         ),
     )
 
@@ -85,13 +203,13 @@ def test_invariance(xyzs, types, atol=1e-6, seed=0):
     np.testing.assert_allclose(r, r2, atol=atol)
 
 
-def test_derivative(xyzs, types, atol=1e-6):
+def test_distance_matrix_and_derivative(xyzs, atol=1e-6):
     _, dr_dx = get_distance_matrix_and_derivative(xyzs)
-    # n = len(xyzs)
-    # dr_dx = dr_dx[:, :, tf.newaxis, :] * (
-    #     tf.eye(n)[tf.newaxis, :, :, tf.newaxis]
-    #     - tf.eye(n)[:, tf.newaxis, :, tf.newaxis]
-    # )
+    n = len(xyzs)
+    dr_dx = dr_dx[:, :, tf.newaxis, :] * (
+        tf.eye(n)[tf.newaxis, :, :, tf.newaxis]
+        - tf.eye(n)[:, tf.newaxis, :, tf.newaxis]
+    )
 
     def fun(x):
         # numdifftools only supports vector to vector functions
@@ -102,3 +220,27 @@ def test_derivative(xyzs, types, atol=1e-6):
 
     num_dr_dx = nd.Jacobian(fun)(xyzs.numpy().flatten()).reshape(dr_dx.shape)
     np.testing.assert_allclose(dr_dx, num_dr_dx, atol=atol)
+
+
+def test_get_distance_matrix_and_full_derivative(xyzs, atol=1e-6):
+    _, dr_dx = get_distance_matrix_and_full_derivative(xyzs)
+
+    def fun(x):
+        # numdifftools only supports vector to vector functions
+        # so first reshape the xyzs array to 2d
+        x = x.reshape(xyzs.shape)
+        # and then flatten the 3d output back to 1d
+        return get_distance_matrix(x).numpy().flatten()
+
+    num_dr_dx = nd.Jacobian(fun)(xyzs.numpy().flatten()).reshape(dr_dx.shape)
+    np.testing.assert_allclose(dr_dx, num_dr_dx, atol=atol)
+
+
+def test_distance_and_pair_types(ragged_xyzs, ragged_types):
+    distances, pair_types, derivative = distances_and_pair_types(
+        ragged_xyzs, ragged_types, 2, 5.1
+    )
+
+    assert distances.shape == (2, None, None, 1)
+    assert pair_types.shape == (2, None, None, 1)
+    assert derivative.shape == (2, None, None, 3)
